882c9d76180862c7aad545a1c9be3684b5ab38f6
VictorClever <hyoukavcss@gmail.com>
2019-03-25 10:59:55 -0300

    Preparando o sistema de particulas das moedas

---
+++ Assets/Scripts/DestroyWithDelay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWithDelay : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public float delay = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, delay);
+        
+    }
+}
+
