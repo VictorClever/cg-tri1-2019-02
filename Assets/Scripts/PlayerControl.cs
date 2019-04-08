2038576aad26ab895c4a93dfb50d592cf46fb534
VictorClever <hyoukavcss@gmail.com>
2019-04-08 09:05:06 -0300

    Tocando o som da moeda

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Coin")) {
             SFXManager.instance.ShowCoinParticles(other.gameObject);
+            AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
             Destroy(other.gameObject);
         }
     }
